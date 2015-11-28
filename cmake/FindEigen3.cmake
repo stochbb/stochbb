f09c913a27ef34c53a95f1f5570f9e8bbb810dc5
Hannes Matuschek <hmatuschek@gmail.com>
2015-11-28 12:48:22 +0100

    ...

---
+++ cmake/FindEigen3.cmake
@@ -0,0 +1,15 @@
+#
+# Try to find Eigen3 include dirs.
+#
+# Eigen is a C++ template library, therefore there are no libraries to find.
+#
+
+find_path(EIGEN3_INCLUDE_DIR Eigen/Eigen PATH_SUFFIXES eigen3)
+
+set(EIGEN3_INCLUDE_DIRS ${EIGEN3_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Eigen3  DEFAULT_MSG
+                                  EIGEN3_INCLUDE_DIRS)
+
+mark_as_advanced(EIGEN3_INCLUDE_DIRS EIGEN3_LIBRARY)
