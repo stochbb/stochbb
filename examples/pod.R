80888a7c7bc381b6feddc433621af145a771506f
Hannes Matuschek <hmatuschek@gmail.com>
2016-04-26 15:04:25 +0200

    Backup

--- examples/pod.R
+++ examples/pod.R
@@ -14,15 +14,16 @@ X2 <- gamma(1,70) %+% d
 #   is simply R = C + X1
 # The resulting distribution of response latecies is an analytic function on (0, infinity).
 Rc <- C %+% X1
-# And for the experimental condition R = C + min(X1, X2).
-#
+
+# And for the experimental condition 
+#   R = C + min(X1, X2).
 # This might be considered as a two parallel proceses X1 (identical to the X1 under control
 # condition) and X2 which is delayed by d=300 ms but once started, much faster than X1.
 # The resulting distribution is not analytic on (0, infinity)! It is identical to the distribution
 # of R under control condition (Rc) on the interval (0, d] and diverges for T>d.
 Re <- C %+% minimum(X1, X2)
 
-Tmin <- 0; Tmax <- 1200; N <- 1000;
+Tmin <- 0; Tmax <- 1200; N <- 2000;
 t   <- seq(Tmin, Tmax, length.out=N);
 Tc  <- array(0, c(N)); Rc$density$eval(Tmin, Tmax, Tc)
 Te  <- array(0, c(N)); Re$density$eval(Tmin, Tmax, Te)
@@ -34,18 +35,20 @@ sam <- new(ExactSampler, c(Rc, Re))
 res <- array(0, c(10000,2))
 sam$sample(res)
 
+# PDFs
 plot(t, Te, type="l", lty=2)
-lines(density(res[,2]), lty=4)
 lines(t, Tc, lty=1)
-lines(density(res[,1]), lty=4)
+abline(v=d, lty=3)
+
+# emp. PDFs (10000 sample)
+plot(density(res[,2]), lty=2)
+lines(density(res[,1]), lty=1)
 abline(v=d, lty=3)
 
 # Survival plots
-#plot(t, 1-TCc, type="l")
-#lines(t, 1-TCe, lty=2)
-#abline(v=d, lty=3)
-
-
+plot(t, 1-TCc, type="l")
+lines(t, 1-TCe, lty=2)
+abline(v=d, lty=3)
 
 # uncomment to plot "zoom" to divergence point.
 #N <- 100000; dt <- (Tmax-Tmin)/N
