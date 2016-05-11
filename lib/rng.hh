2abd272f6e999a773ce1a2396085193bc7f7c3d1
Hannes Matuschek <hmatuschek@gmail.com>
2016-05-11 14:31:31 +0200

    Fixed docs.

--- lib/rng.hh
+++ lib/rng.hh
@@ -6,8 +6,8 @@
 namespace stochbb {
 
 /** Singleton class of the random number generator using the Mersenne twister (@c std::mt19937_64)
- * RNG. This class can be used in conjecture with the distributions defined in C++ std. lib
- * <random>.
+ * RNG. This class can be used in conjecture with the distributions defined in C++ std. libs
+ * 'random'.
  * @ingroup math */
 class RNG: public std::mt19937_64
 {
@@ -33,37 +33,25 @@ template <typename RealType=double>
 class inverse_gamma_distribution: public gamma_distribution<RealType>
 {
 public:
+  /** Constructs a new inverse gamma sampler with the given parameters. */
   explicit inverse_gamma_distribution(RealType alpha = 1, RealType beta = 1)
     : gamma_distribution<RealType>(alpha, beta) {}
 
+  /** Constructs a new inverse gamma sampler with the given parameters. */
   explicit inverse_gamma_distribution(const typename gamma_distribution<RealType>::param_type& p)
     : gamma_distribution<RealType>(p) {}
 
   /** Generates inverse gamma distributed random values using the given RNG. */
-  template<class URNG> RealType operator()(URNG& g);
+  template<class URNG> RealType operator()(URNG& g) {
+    return 1./gamma_distribution<RealType>::operator() (g);
+  }
+
   /** Generates inverse gamma distributed random values using the given RNG and parameters. */
-  template<class URNG> RealType operator()(URNG& g, const typename gamma_distribution<RealType>::param_type& parm);
+  template<class URNG> RealType operator()(URNG& g, const typename gamma_distribution<RealType>::param_type& param) {
+    return 1./gamma_distribution<RealType>::operator() (g, param);
+  }
 };
 
-
-/* ********************************************************************************************* *
- * Template implementation
- * ********************************************************************************************* */
-template<class _RealType>
-template<class _URNG>
-_RealType
-inverse_gamma_distribution<_RealType>::operator() (_URNG &__g, const typename gamma_distribution<_RealType>::param_type &__p)
-{
-  return 1./gamma_distribution<_RealType>::operator() (__g, __p);
 }
 
-template<class _RealType>
-template<class _URNG>
-_RealType
-inverse_gamma_distribution<_RealType>::operator() (_URNG &__g)
-{
-  return 1./gamma_distribution<_RealType>::operator() (__g);
-}
-
-}
 #endif // __SBB_RNG_HH__
